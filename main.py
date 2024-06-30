e71fced755ada22250131fc84f297debda96ab42
Mike Silva <mike.a.silva@gmail.com>
2024-06-30 18:58:29 -0400

    Various updates

--- main.py
+++ main.py
@@ -3,11 +3,27 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import datetime
-from dotenv import load_dotenv, find_dotenv
+#from dotenv import load_dotenv, find_dotenv
+import json
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
 
 # https://medium.com/@chodvadiyasaurabh/integrating-stripe-payment-gateway-with-fastapi-a-comprehensive-guide-8fe4540b5a4
 
-app = FastAPI()
+metadata_tags = [
+    {
+        "name": "web",
+        "description": "Webpages and related APIs.",
+    },
+]
+
+app = FastAPI(
+    title="Restoration Run",
+    version="2024.06.30",
+    openapi_tags=metadata_tags,
+)
 app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 app.mount("/images", StaticFiles(directory="images"), name="images")
 
@@ -15,14 +31,13 @@ templates = Jinja2Templates(directory="templates")
 
 current_year = str(datetime.datetime.today()).split("-")[0]
 
-@app.get("/", response_class=HTMLResponse)
-def root(request: Request):
+@app.get("/", response_class=HTMLResponse, tags=["web"])
+def website(request: Request):
     program_template = "index.html"
     return templates.TemplateResponse(program_template, {"request": request, "year": current_year})
 
-'''
+
 if __name__ == "__main__":
     import uvicorn
     
     uvicorn.run(app, host="0.0.0.0", port=8000)
-'''
