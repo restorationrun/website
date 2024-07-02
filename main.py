f7e45d39e6e0381a00460f781b9d85b2e962f9e5
Mike Silva <mike.a.silva@gmail.com>
2024-07-02 19:41:52 -0400

    Updated templates

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import datetime
 #from dotenv import load_dotenv, find_dotenv
-#import json
-#from google.auth.transport.requests import Request
-#from google_auth_oauthlib.flow import InstalledAppFlow
-#from googleapiclient.discovery import build
-#from google.oauth2 import service_account
 
 # https://medium.com/@chodvadiyasaurabh/integrating-stripe-payment-gateway-with-fastapi-a-comprehensive-guide-8fe4540b5a4
 
@@ -36,9 +31,18 @@ def website(request: Request):
     program_template = "index.html"
     return templates.TemplateResponse(program_template, {"request": request, "year": current_year})
 
-'''
+@app.get("/race-course", response_class=HTMLResponse, tags=["web"])
+def map(request: Request):
+    program_template = "map.html"
+    return templates.TemplateResponse(program_template, {"request": request, "year": current_year})
+
+@app.get("/what-is-the-restoration", response_class=HTMLResponse, tags=["web"])
+def map(request: Request):
+    program_template = "restoration.html"
+    return templates.TemplateResponse(program_template, {"request": request, "year": current_year})
+
+
 if __name__ == "__main__":
     import uvicorn
     
     uvicorn.run(app, host="0.0.0.0", port=8000)
-'''
