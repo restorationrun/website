f2a27c4d757588e3b20c293dff7c3bf5246e98fe
Mike Silva <mike.a.silva@gmail.com>
2024-07-02 19:44:25 -0400

    Update main

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import datetime
-#from dotenv import load_dotenv, find_dotenv
+
+# from dotenv import load_dotenv, find_dotenv
 
 # https://medium.com/@chodvadiyasaurabh/integrating-stripe-payment-gateway-with-fastapi-a-comprehensive-guide-8fe4540b5a4
 
@@ -26,23 +27,33 @@ templates = Jinja2Templates(directory="templates")
 
 current_year = str(datetime.datetime.today()).split("-")[0]
 
+
 @app.get("/", response_class=HTMLResponse, tags=["web"])
 def website(request: Request):
     program_template = "index.html"
-    return templates.TemplateResponse(program_template, {"request": request, "year": current_year})
+    return templates.TemplateResponse(
+        program_template, {"request": request, "year": current_year}
+    )
+
 
 @app.get("/race-course", response_class=HTMLResponse, tags=["web"])
 def map(request: Request):
     program_template = "map.html"
-    return templates.TemplateResponse(program_template, {"request": request, "year": current_year})
+    return templates.TemplateResponse(
+        program_template, {"request": request, "year": current_year}
+    )
+
 
 @app.get("/what-is-the-restoration", response_class=HTMLResponse, tags=["web"])
-def map(request: Request):
+def restoration(request: Request):
     program_template = "restoration.html"
-    return templates.TemplateResponse(program_template, {"request": request, "year": current_year})
+    return templates.TemplateResponse(
+        program_template, {"request": request, "year": current_year}
+    )
 
 
+"""
 if __name__ == "__main__":
     import uvicorn
-    
     uvicorn.run(app, host="0.0.0.0", port=8000)
+"""
